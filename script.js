6c44f25ac2ee2135197dc4b7e714b9427095d23c
Richard98PL <41301282+Richard98PL@users.noreply.github.com>
2019-12-06 13:25:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,9 +4,10 @@ function myFunction(){
     let link = "http://certification.secure.force.com/certificationsite/services/apexrest/credential?searchString=";
     let IDPiotr = "tIIO/SJ+dSo2SWgXFUW7b5tNdsmyRajbKp0PMnJCizGscFNuhJwiI8elmiwcf;+bu";
     let ID = "whCXOIEC2E+2kpxO14AWUEZO7op3wz6vqGohfpGp80jnQkvwJ8CCRRWVs3DG0hA7";
+    let IdPietrekDziwne = "nUpdVuAKL00gfg6BN+S90VKR3fYT+c0XgIh5C6/BgXg+A9KH+4JTQfGKQzQ8c/0M";
     
     let http_request = new XMLHttpRequest();
-    http_request.open("GET", "https://cors-anywhere.herokuapp.com/"+link+ID);
+    http_request.open("GET", "https://cors-anywhere.herokuapp.com/"+link+IdPietrekDziwne);
     http_request.send();
     
     http_request.onreadystatechange = function(){
